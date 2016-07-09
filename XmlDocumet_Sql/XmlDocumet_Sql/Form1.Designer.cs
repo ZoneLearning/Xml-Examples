e7479707b48749b8b0e4352bbb1e1867fc74ce92
ZoneLearning <myselflearningzone@gmail.com>
2016-07-10 01:07:06 +0300

    XmlDocumet_Sql--> Proje Oluşturuldu.

---
+++ XmlDocumet_Sql/XmlDocumet_Sql/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace XmlDocumet_Sql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSupplier = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSupplier
+            // 
+            this.btnSupplier.Location = new System.Drawing.Point(24, 13);
+            this.btnSupplier.Name = "btnSupplier";
+            this.btnSupplier.Size = new System.Drawing.Size(138, 23);
+            this.btnSupplier.TabIndex = 0;
+            this.btnSupplier.Text = "XML\'e  Tedarikçi Yaz";
+            this.btnSupplier.UseVisualStyleBackColor = true;
+            this.btnSupplier.Click += new System.EventHandler(this.btnSupplier_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnSupplier);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSupplier;
+    }
+}
+
---
+++ XmlDocumet_Sql/XmlDocumet_Sql/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XmlDocumet_Sql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSupplier_Click(object sender, EventArgs e)
+        {
+            XmlDocument xdoc = new XmlDocument();
+            XmlElement root = xdoc.CreateElement("Tedarikciler");
+            SqlConnection conSuppliers = new SqlConnection("Server=.;Database=NORTHWND;Integrated Security=true;MultipleActiveResultSets=true");
+            SqlCommand comSuppliers = new SqlCommand("Select * from Suppliers", conSuppliers);
+            conSuppliers.Open();
+            SqlDataReader suppliersRead = comSuppliers.ExecuteReader();
+            while (suppliersRead.Read())
+            {
+                XmlElement supplier = xdoc.CreateElement("tedarikci");
+
+                root.AppendChild(supplier);
+            }
+            conSuppliers.Close();
+
+        }
+    }
+}
