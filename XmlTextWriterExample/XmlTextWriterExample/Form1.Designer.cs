40b1f490f3d1481ff20961fe22e5b463242c8b1c
ZoneLearning <myselflearningzone@gmail.com>
2016-07-09 22:41:34 +0300

    XmlTextWriter Example--> NORTHWND veri tabanındaki Categories tablosu XML'e XmlTextWriter kullanılarak yazıldı.

--- XmlTextWriterExample/XmlTextWriterExample/Form1.Designer.cs
+++ XmlTextWriterExample/XmlTextWriterExample/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnXMLWrite = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnXMLWrite
+            // 
+            this.btnXMLWrite.Location = new System.Drawing.Point(24, 12);
+            this.btnXMLWrite.Name = "btnXMLWrite";
+            this.btnXMLWrite.Size = new System.Drawing.Size(137, 23);
+            this.btnXMLWrite.TabIndex = 0;
+            this.btnXMLWrite.Text = "XmlTextWriter ile Yaz\r\n";
+            this.btnXMLWrite.UseVisualStyleBackColor = true;
+            this.btnXMLWrite.Click += new System.EventHandler(this.btnXMLWrite_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnXMLWrite);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnXMLWrite;
     }
 }
 
--- XmlTextWriterExample/XmlTextWriterExample/Form1.cs
+++ XmlTextWriterExample/XmlTextWriterExample/Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace XmlTextWriterExample
 {
@@ -15,6 +17,28 @@ namespace XmlTextWriterExample
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnXMLWrite_Click(object sender, EventArgs e)
+        {
+            XmlTextWriter xwriter = new XmlTextWriter("Categories.xml", Encoding.Unicode);
+
+            SqlConnection connection = new SqlConnection("Server=.;Database=NORTHWND;;Integrated Security=true");
+            SqlCommand command = new SqlCommand("select * from Categories", connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            xwriter.WriteStartElement("Kategoriler");
+
+            while (reader.Read())
+            {
+                xwriter.WriteStartElement("Kategori");
+                xwriter.WriteElementString("Adi", reader["CategoryName"].ToString());
+                xwriter.WriteElementString("Aciklama", reader["Description"].ToString());
+                xwriter.WriteEndElement();
+            }
+            connection.Close();
+            xwriter.WriteEndElement();
+            xwriter.Close();
         }
     }
 }
