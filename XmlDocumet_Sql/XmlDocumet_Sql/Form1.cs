2d58700d87d24c96fe06f8129ac21f5a4eb60c71
ZoneLearning <myselflearningzone@gmail.com>
2016-07-10 08:58:50 +0300

    XmlDocument ile Sql'den veri çekip XML'e yazma.-->InnerText yerine XmlAtribute olarak

--- XmlDocumet_Sql/XmlDocumet_Sql/Form1.cs
+++ XmlDocumet_Sql/XmlDocumet_Sql/Form1.cs
@@ -23,17 +23,30 @@ namespace XmlDocumet_Sql
         {
             XmlDocument xdoc = new XmlDocument();
             XmlElement root = xdoc.CreateElement("Tedarikciler");
-            SqlConnection conSuppliers = new SqlConnection("Server=.;Database=NORTHWND;Integrated Security=true;MultipleActiveResultSets=true");
+            SqlConnection conSuppliers = new SqlConnection("Server=.;Database=NORTHWND;Integrated Security=true;MultipleActiveResultSets=true;");
             SqlCommand comSuppliers = new SqlCommand("Select * from Suppliers", conSuppliers);
             conSuppliers.Open();
             SqlDataReader suppliersRead = comSuppliers.ExecuteReader();
             while (suppliersRead.Read())
             {
-                XmlElement supplier = xdoc.CreateElement("tedarikci");
+                XmlElement supplier = xdoc.CreateElement("supplier");
+                XmlAttribute company = xdoc.CreateAttribute("company");
+                company.Value = suppliersRead["CompanyName"].ToString();
+                XmlAttribute phone = xdoc.CreateAttribute("phone");
+                phone.Value = suppliersRead["Phone"].ToString();
+                XmlAttribute contactName = xdoc.CreateAttribute("contact");
+                contactName.Value = suppliersRead["ContactName"].ToString();
+
+                supplier.Attributes.Append(company);
+                supplier.Attributes.Append(phone);
+                supplier.Attributes.Append(contactName);
 
                 root.AppendChild(supplier);
             }
             conSuppliers.Close();
+
+            xdoc.AppendChild(root);
+            xdoc.Save("suppliers.xml");
 
         }
     }
